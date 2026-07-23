f4d408fc1a511025752f4597c1ca5186e1fb4b74
Fatima Raazia <rf3932090@gmail.com>
2026-07-23 18:39:43 +0330

    Correcting some errors

--- services/analyzer_service.py
+++ services/analyzer_service.py
@@ -1,8 +1,8 @@
-from google import genai
 from dotenv import load_dotenv
 import os
 import json
 
+from .openrouter_client import client
 from analysis.analysis_prompt import ANALYSIS_PROMPT
 
 load_dotenv()
@@ -11,24 +11,24 @@ load_dotenv()
 class PromptAnalysisService:
 
     def __init__(self):
-
-        self.client = genai.Client(
-            api_key=os.getenv("GEMINI_API_KEY")
-        )
-
-        self.model = os.getenv("ANALYZER_MODEL")
-
-
+        self.client = client
 
     def analyze(self, request: dict):
         request_json = json.dumps(request, indent=2)
 
-        response = self.client.models.generate_content(
-            model=self.model,
-            contents=[
-                ANALYSIS_PROMPT,
-                request_json
-            ]
+        response = self.client.chat.completions.create(
+            model="google/gemini-2.5-flash",
+            messages=[
+                {
+                    "role": "system",
+                    "content": ANALYSIS_PROMPT,
+                },
+                {
+                    "role": "user",
+                    "content": request_json,
+                },
+            ],
+            temperature=0,
         )
 
-        return json.loads(response.text)
+        return json.loads(response.choices[0].message.content)
